c4d36c82d163998a22e71f0e929ff610610bab70
Iris94 <mirza.iris94@gmail.com>
2023-10-15 00:04:31 +0200

    Did a first sketch up of landing page

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,14 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'main-shade' : '#011C26',
+				'light-shade' : '#F2AA6B',
+				'medium-shade' : '#D97941',
+				'dark-shade' : '#592A19',
+			  },
+		},
 	},
 	plugins: [],
 }
